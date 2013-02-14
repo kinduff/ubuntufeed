c6b670d1013ae3b0de2f3d3ffb6ca5bac3431f3b
Kinduff <abarcadabra@gmail.com>
2013-02-14 14:56:10 -0600

    Agrega logo a blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ActiveRecord::Base
-  attr_accessible :title, :url
+  attr_accessible :title, :url, :logo
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130214205445) do
+
+  create_table "blogs", :force => true do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+  end
+
+end
