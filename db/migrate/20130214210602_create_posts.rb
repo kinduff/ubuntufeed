f8635b29c06276c99720c55e0e70fee08de7e2aa
kinduff <abarcadabra@gmail.com>
2013-02-15 17:37:29 -0600

    Quita campo comments de db original

--- db/migrate/20130214210602_create_posts.rb
+++ db/migrate/20130214210602_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.string :link
-      t.string :comments
       t.timestamp :pubdate
       t.text :description
     end
