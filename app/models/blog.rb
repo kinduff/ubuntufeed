a0036b8e20779353baabed83201d780caecd65f1
kinduff <abarcadabra@gmail.com>
2013-02-15 14:42:08 -0600

    Relacion entre modelos

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ActiveRecord::Base
   attr_accessible :title, :url, :logo
   has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   attr_accessible :comments, :description, :link, :pubdate, :title
+  belongs_to :blog
 end
