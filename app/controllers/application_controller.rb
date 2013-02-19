8ccafecfbd59e4c440650d8543239556e48c57a1
Kinduff <abarcadabra@gmail.com>
2013-02-19 13:06:02 -0600

    Thank you, Facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :redirect_main_domain if Rails.env.production?
-  def redirect_main_domain
-    dominio = 'ubuntufeed.tk'
-    if request.env['HTTP_HOST'] != dominio
-      redirect_to "http://#{dominio}#{request.fullpath}"
-    end
-  end
+  # En lo que compro un dominio cancelo la redirección
+  # before_filter :redirect_main_domain if Rails.env.production?
+  # def redirect_main_domain
+  #   dominio = 'ubuntufeed.tk'
+  #   if request.env['HTTP_HOST'] != dominio
+  #     redirect_to "http://#{dominio}#{request.fullpath}"
+  #   end
+  # end
 end
