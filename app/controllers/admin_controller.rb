1d309582945662eec8df12b73b45cd1792420a70
Alejandro AR <abarcadabra@gmail.com>
2013-10-18 18:57:00 -0500

    tmp fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -71,7 +71,6 @@ class AdminController < ApplicationController
                   end
                   pubdate = entry.published
                   blog_id = Blog.find_by_feed_url(feed_url).id
-                  short = short link
                   tweets << "#{title} #{short}"
                   posts << {
                     :title => title,
