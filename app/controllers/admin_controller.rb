e8c5532f08ee4bad5353d5ebe7dcac9bb11ba6f5
Alejandro AR <abarcadabra@gmail.com>
2013-10-22 12:06:34 -0500

    Fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   include AdminHelper
-  http_basic_authenticate_with :name => "123", :password => "123"
+  http_basic_authenticate_with :name => ENV['U'], :password => ENV['U']
   def index
     @blogs = Blog.order('title ASC').all
   end
